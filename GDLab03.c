5fad0bdd7ee5b43ab2f9c49eaa9869d22002b5ff
Nguyen Cat Tuong <nguyentuong609@gmail.com>
2025-11-30 23:16:31 +0700

    Add files via upload

---
+++ GDLab03.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+float kiemTraDiem(float x){
+    if (x >= 9) printf("Hoc luc xuat sac\n");
+    else if (x >= 8) printf("Hoc luc gioi\n");
+    else if (x >= 6.5)printf("Hoc luc kha\n");
+    else if (x >= 5)printf("Hoc luc trung binh\n");
+    else if (x >= 3.5) printf("Hoc luc yeu\n");
+    else printf("Hoc luc kem\n");
+
+    return 0;
+}
+
+float phuongTrinh_2_1(float a, float b){
+    if (a == 0){
+        if(b == 0) printf("Phuong Trinh co vo so nghiem");
+        else printf("Phuong trinh vo nghiem");
+    }
+    else{
+        float x = -b / a;
+        printf("x la: %.2f\n",x);}
+}
+
+float phuongTrinh_2_2(float a, float b, float c){
+    if(a == 0){
+        float x = -c / b;
+        printf("Phuong trinh thanh bx + c = 0\n");
+        printf("x la %.2f\n", x);
+    }
+    else{
+        float delta = (b*b) - 4*a*c;
+        if(delta < 0)printf("Phuont trinh vo nghiem\n");
+        else if(delta == 0){float x = -b/(2*a); printf("Phuong trinh co nghiem kep x1 = x2 = %.2f\n", x);}
+        else{
+            float x1 = (-b + sqrt(delta)) / (2 * a);
+            float x2 = (-b - sqrt(delta)) / (2 * a);
+            printf("Phuong trinh co 2 nghiem rieng biet\nx1 la: %.2f va x2 la: %.2f\n", x1, x2);
+        }
+    }
+}
+
+int main(){
+    int choice;
+    do{
+        printf("Nhap so de chon chuc nang\n");
+        printf("Chuc nang 1: Tinh hoc luc\n");
+        printf("Chuc nang 2: Giai phuong trinh 2.1\n");
+        printf("Chuc nang 3: Giai phuong trinh 2.2\n");
+        printf("Chuc nang 4: Tinh tien dien\n");
+        printf("Chuc nang 0: EXIT\n");
+        printf("Chon so: "); 
+
+        if (scanf("%d", &choice) != 1){
+            printf("Phien chon so tu 1 toi 4\n");
+            while (getchar() != '\n' );
+            continue;
+        }
+
+        switch(choice){
+            case 1:{
+                printf("Ban da chon CN1: tinh hoc luc\n");
+                float diem;
+                printf("Phien ban nhap diem: "); scanf("%f", &diem);
+                kiemTraDiem(diem);
+                break;
+            }
+            case 2:{
+                printf("Ban da chon CN2: bac 1\n");
+                float a, b;
+                printf("Phuong trinh de giai ax + b = 0\n");
+                printf("Nhap so a: "); scanf("%f", &a);
+                printf("Nhap so b: "); scanf("%f", &b);
+
+                phuongTrinh_2_1(a,b);
+                break;
+            }
+            case 3:{
+                printf("Ban da chon CN3: bac 2\n");
+                float a, b, c;
+                printf("Phuong trinh de giai ax2 + bx + c = 0\n");
+                printf("Nhap so a: "); scanf("%f", &a);
+                printf("Nhap so b: "); scanf("%f", &b);
+                printf("Nhap so c: "); scanf("%f", &c);
+
+                phuongTrinh_2_2(a,b,c);
+
+                break;
+            }
+            case 4:{
+                break;
+            }
+            case 0:{
+                printf("Goodbye!");
+                break;
+            }
+            default:{
+                printf("Phien cho so tu 1 toi 4");
+                break;
+            }
+
+        }
+
+    }
+    while(choice != 0);
+
+    return 0;
+}
